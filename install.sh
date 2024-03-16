9f7e4493fc81e0879eb12c8f06d87e0785ecc843
augusto <augusto@iMac-Pro.local>
2024-03-16 13:05:12 -0300

    install php, mysql, git, etc

---
+++ install.sh
@@ -0,0 +1,7 @@
+
+#PHP, Mysql e Extensões
+echo "\e[1;33mInstalando PHP, MySQL e Extensões..."
+sudo apt update && sudo apt install -y php php-ctype php-curl php-dom php-fileinfo php-filter php-hash php-mbstring php-opcache php-pdo php-session php-tokenizer php-xml git nginx mariadb curl
+
+echo "\e[1;33mA instalação está em andamento. Isso pode levar algum tempo..."
+
