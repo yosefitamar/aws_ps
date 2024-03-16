932d3af094808ab5ba3e0dfc3bea016766e2f398
augusto <augusto@iMac-Pro.local>
2024-03-16 16:23:33 -0300

    fix

--- install.sh
+++ install.sh
@@ -1,7 +1,28 @@
 
 #PHP, Mysql e Extensões
-echo "\e[1;33mInstalando PHP, MySQL e Extensões..."
+echo "Instalando PHP, MySQL e Extensões..."
 sudo apt update && sudo apt install -y php php-ctype php-curl php-dom php-fileinfo php-filter php-hash php-mbstring php-opcache php-pdo php-session php-tokenizer php-xml git nginx mariadb curl
 
-echo "\e[1;33mA instalação está em andamento. Isso pode levar algum tempo..."
+echo "A instalação está em andamento. Isso pode levar algum tempo..."
 
+# Solicitar a senha root do MariaDB
+read -s -p "Digite a senha root para o MariaDB (deixe em branco para senha vazia): " db_root_password
+echo
+
+# Exibir mensagem de configuração segura do MariaDB
+echo -e "Configurando o MariaDB de forma segura..."
+
+# Executar a configuração segura do MariaDB automaticamente
+sudo mysql_secure_installation <<EOF
+
+Y
+$db_root_password
+$db_root_password
+Y
+Y
+Y
+Y
+EOF
+
+# Exibir mensagem de conclusão
+echo -e "\e[1;32mConfiguração do MariaDB concluída com sucesso!\e[0m"
