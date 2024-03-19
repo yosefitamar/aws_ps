23d15a002c0d0cf432e8d6da36c5df234daa62ab
augusto <augusto@iMac-Pro.local>
2024-03-19 20:26:38 -0300

    final commit to first release

--- install.sh
+++ install.sh
@@ -1,18 +1,16 @@
 #############
-# Variáveis #
+# Variables #
 #############
 INFO='\033[1;33m'
 SUCCESS='\033[0;32m'
 ALERT='\033[41m'
 WARNING='\033[45m'
 NC='\033[0m'
-#DEFAULT_REP=
 REP_URL='https://github.com/'
 
 echo -e "${INFO}Digite a versão do PHP:${NC}"
 read PHP_VER
 
-# Verifica se o parâmetro -nogit foi passado
 while [[ "$1" != "" ]]; do
     case $1 in
         -nogit )
@@ -26,22 +24,18 @@ while [[ "$1" != "" ]]; do
     shift
 done
 
-#PHP, Mysql e Extensões
 echo -e "${INFO}Instalando PHP, MySQL e Extensões...${NC}"
 sudo apt update && sudo apt install -y php php$PHP_VER-common php$PHP_VER-mysql php$PHP_VER-intl php$PHP_VER-fpm php$PHP_VER-xml php$PHP_VER-zip git nginx mariadb-server mariadb-client curl unzip
 
 echo -e "${INFO}A instalação está em andamento. Isso pode levar algum tempo...${NC}"
 
-# Solicitar a senha root do MariaDB
 echo -e "${INFO}Digite a senha root para o MariaDB (deixe em branco para senha vazia):${NC} " 
 stty -echo
 read db_root_password
 stty echo
 
-# Exibir mensagem de configuração segura do MariaDB
 echo -e "${INFO}Configurando o MariaDB de forma segura...${NC}"
 
-# Executar a configuração segura do MariaDB automaticamente
 sudo mysql_secure_installation <<EOF
 
 Y
@@ -53,38 +47,30 @@ Y
 Y
 EOF
 
-# Exibir mensagem de conclusão
 echo -e "${SUCCESS}Configuração do MariaDB concluída com sucesso!${NC}"
 
-# Solicita ao usuário o nome do banco de dados
 echo -e "${INFO}Digite o nome do banco de dados:${NC}"
 read database_name
 
-# Verifica se o nome do banco de dados foi fornecido
 if [ -z "$database_name" ]; then
     echo "${WARNING}Atenção, banco de dados não foi informado! Nenhum banco será criado.{$NC}"
 else
     if sudo mysql -e "SHOW DATABASES LIKE '$database_name'" | grep -q "$database_name"; then
         echo -e "${WARNING}Já existe um banco de dados com o nome '$database_name'.${NC}"
     else
-        # Cria o banco de dados com o nome fornecido pelo usuário
         sudo mysql -e "CREATE DATABASE $database_name"
         echo -e "${SUCCESS}Banco de dados '$database_name' criado com sucesso.${NC}"
     fi
 fi
 
-# Loop para solicitar o nome do novo usuário
 while true; do
-    # Solicita ao usuário o nome do novo usuário
     echo -e "${INFO}Informe o usuário do BD ('q' para sair):${NC}"
     read db_username
 
-    # Verifica se o usuário digitou 'q' para sair
     if [ "$db_username" == "q" ]; then
         exit 0
     fi
 
-    # Verifica se o nome do novo usuário está vazio
     if [ -z "$db_username" ]; then
         echo -e "${WARNING}Atenção, o nome do usuário não pode ser vazio!${NC}"
     else
@@ -92,27 +78,22 @@ while true; do
     fi
 done
 
-# Solicita ao usuário a senha para o novo usuário
 echo -e "${INFO}Digite a senha para o novo usuário:${NC}"
 stty -echo
 read db_password
 stty echo
 
-# Cria o novo usuário com a senha fornecida
 sudo mysql -e "CREATE USER '$db_username'@'localhost' IDENTIFIED BY '$db_password';"
 echo -e "${SUCCESS}Usuário '$db_username' criado com sucesso.${NC}"
 
-# Concede ao novo usuário privilégios para acessar e gerenciar o banco de dados
 sudo mysql -e "GRANT ALL PRIVILEGES ON $database_name.* TO '$db_username'@'localhost';"
 sudo mysql -e "FLUSH PRIVILEGES;"
 echo -e "${SUCCESS}Privilégios concedidos para o usuário '$db_username' no banco de dados '$database_name'.${NC}"
 
-# Exibe informações sobre o novo banco de dados e usuário
 echo -e "${INFO}Banco de dados: $database_name"
 echo -e "Usuário: $db_username"
 echo -e "Senha: ********${NC}"
 
-# Instalação do Composer
 if [ -x "$(command -v composer)" ]; then
     echo -e "${INFO}Composer já instalado. Pulando etapa.${NC}"
 else
@@ -122,7 +103,6 @@ else
     rm composer-setup.php
 fi
 
-# Verifica se o Composer foi instalado com sucesso
 if [ $? -eq 0 ]; then
     echo -e "${SUCCESS}Composer instalado com sucesso.${NC}"
 else
@@ -130,7 +110,6 @@ else
     exit 1
 fi
 
-# Solicita ao usuário o nome da pasta para o projeto
 project_folder=""
 while [ -z "$project_folder" ]; do
     echo -e "${INFO}Digite o nome da pasta do projeto ('q' para sair):${NC}"
@@ -148,9 +127,7 @@ echo -e "${SUCCESS}Pasta do projeto definida como: $project_folder${NC}"
 
 project_path="/var/www/$project_folder"
 
-#Clone do repositório git
 if ! [ "$no_git" = true ]; then
-    # Altera permissões da pasta www
     sudo chmod o+w /var/www
 
     echo -e "${INFO}Digite o repositório no github (e.g. yosef/myproject.git):${NC}"
@@ -191,7 +168,6 @@ fi
 
 env_file="$project_path/.env"
 
-# Substitui os valores das variáveis no arquivo .env
 sed -i "s/^APP_NAME=.*/APP_NAME=$project_folder/" "$env_file"
 sed -i "s/^DB_DATABASE=.*/DB_DATABASE=$database_name/" "$env_file"
 sed -i "s/^DB_USERNAME=.*/DB_USERNAME=$db_username/" "$env_file"
@@ -210,10 +186,8 @@ echo -e "${SUCCESS}Permissões alteradas com sucesso!${NC}"
 echo -e "${INFO}Ajustando o NGINX...${NC}"
 sudo mv "/etc/nginx/sites-available/default" "/etc/nginx/sites-available/backup"
 
-# Define o caminho para o arquivo de configuração do Nginx
 nginx_config="/etc/nginx/sites-available/default"
 
-# Conteúdo do arquivo de configuração
 nginx_content="server {
     listen 80;
     listen [::]:80;
@@ -247,10 +221,8 @@ nginx_content="server {
     }
 }"
 
-# Cria o arquivo de configuração
 echo "$nginx_content" | sudo tee "$nginx_config" > /dev/null
 
-# Verifica se o arquivo foi criado com sucesso
 if [ -f "$nginx_config" ]; then
     echo -e "${SUCCESS}Arquivo de configuração do Nginx criado com sucesso em: $nginx_config ${NC}"
 else
